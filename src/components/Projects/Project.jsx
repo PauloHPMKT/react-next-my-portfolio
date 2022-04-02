f2db26a59c753c232ac4efc8522d0c8d354d0442
Paulo Sergio <pauloserg861@gmail.com>
2022-04-01 22:56:13 -0300

    adicionando estrutura Project

---
+++ src/components/Projects/Project.jsx
@@ -0,0 +1,14 @@
+
+import { Section, SectionDivider, SectionSubTitle } from '../../styles/globalComponentsStyles'
+
+export default function Projects() {
+  return(
+    <Section id='projects'>
+      <SectionDivider />
+      <SectionSubTitle>
+        Projetos
+      </SectionSubTitle>
+      <p>oi</p>
+    </Section>
+  )
+}
