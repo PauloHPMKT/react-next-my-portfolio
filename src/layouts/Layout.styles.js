5a4468603a7f446297f6443a3529234472c1a79f
Paulo Sergio <pauloserg861@gmail.com>
2022-04-17 21:35:56 -0300

    adicionando responsividade

--- src/layouts/Layout.styles.js
+++ src/layouts/Layout.styles.js
@@ -3,4 +3,15 @@ import styled from 'styled-components'
 export const Container = styled.div `
     max-width: 1280px;
     margin: 0 auto;
+
+    @media ${(props => props.theme.breakpoints.xxl)} {
+        max-width: 85%;
+    }
+    @media ${(props => props.theme.breakpoints.lg)} {
+        max-width: 90%;
+    }
+    @media ${(props => props.theme.breakpoints.sm)} {
+        max-width: 95%;
+    }
+
 `
