a7d8e8676c7d4ba610e7d6c21be0b095333e42f9
Paulo Sergio <pauloserg861@gmail.com>
2022-04-17 21:36:26 -0300

    adicionando breakpoints

--- src/theme/default.js
+++ src/theme/default.js
@@ -1,23 +1,30 @@
 export default {
-    // Temp fonts
-    fonts: {
-      title: "Open Sans, sans-serif",
-      main: "Open Sans, sans-serif",
-      secondary_size: "20px",
-      text: "24px"
-    },
-    // Colors for layout
-    colors: {
-      primary1: "#b10101",
-      background1: "#000",
-      text_color: "#cecece",
-    },
-    // Breakpoints for responsive design
-    breakpoints: {
-      xs: 'screen and (max-width: 450px)',
-      sm: 'screen and (max-width: 640px)',
-      md: 'screen and (max-width: 768px)',
-      lg: 'screen and (max-width: 1024px)',
-      xl: 'screen and (max-width: 1280px)'
-    },
+  // Temp fonts
+  fonts: {
+    title: "Open Sans, sans-serif",
+    main: "Open Sans, sans-serif",
+    secondary_size: "20px",
+    text: "24px"
+  },
+  // Colors for layout
+  colors: {
+    primary1: "#b10101",
+    secondary_color: "#fff",
+    background1: "#000",
+    text_color: "#cecece",
+  },
+  // Breakpoints for responsive design
+  breakpoints: {
+    //default screen-size
+    xs: 'screen and (max-width: 450px)',
+    sm: 'screen and (max-width: 640px)',
+    md: 'screen and (max-width: 770px)',
+    lg: 'screen and (max-width: 1024px)',
+    xl: 'screen and (max-width: 1280px)',
+    //setting breakpoints
+    xxl: 'screen and (max-width: 1507px)',
+    mxl: 'screen and (max-width: 1350px)',
+    mlg: 'screen and (max-width: 1170px)',
+    msm: 'screen and (max-width: 1000px)'
+  },
 }
