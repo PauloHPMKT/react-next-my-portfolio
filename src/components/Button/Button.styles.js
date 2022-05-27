a077fac07016d13c4d9dc170a5a5a3ea8d5ed44d
Paulo Sergio <pauloserg861@gmail.com>
2022-05-27 20:12:53 -0300

    adicionando theme a estrutura de estilos

--- src/components/Button/Button.styles.js
+++ src/components/Button/Button.styles.js
@@ -7,15 +7,15 @@ export const ButtonMain = styled.button `
     border: 3px solid #fff;
     border-radius: 25px;
     font-size: 23px;
-    color: #fff;
+    color: ${props => props.theme.colors.secondary_color};
     cursor: pointer;
     transition: .4s ease;
     background-color:transparent;
 
     &:hover {
-        background-color: #b10101;
-        border: 3px solid #b10101;
-        box-shadow: 0 0 15px #b10101;
+        background-color: ${props => props.theme.colors.primary1};
+        border: 3px solid ${props => props.theme.colors.primary1};
+        box-shadow: 0 0 15px ${props => props.theme.colors.primary1};
     }
 `
 
