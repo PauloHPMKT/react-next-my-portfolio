2df2fd133dd9fb0ed99d1a07f6f09c92a3641bff
Paulo Sergio <pauloserg861@gmail.com>
2022-05-27 18:38:03 -0300

    corrigindo arquivo documento

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -30,7 +30,7 @@ export default class MyDocument extends Document {
   }
   render() {
     return (
-      <Html lang='pt-br'>
+      <Html lang='pt'>
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
           <meta name="title" content="Paulo Sérgio | Frontend Dev" />
