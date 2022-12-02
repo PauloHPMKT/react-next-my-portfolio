714a1e991c0aa033a105e11c33ed16f2078a200c
Paulo Sergio <pauloserg861@gmail.com>
2022-12-02 20:22:51 -0300

    configurando nextconfig

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,24 +1,23 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
 
-import Navigation from '../Navigation'
-
-import { HeaderContainer, LinkHeader } from './Header.styles'
+import Navigation from "../Navigation";
+import { HeaderContainer, LinkHeader } from "./Header.styles";
 
 export default function Header() {
-  return(
-    <HeaderContainer>
-      <Link href="/" passHref>
-        <LinkHeader>
-          <Image 
-            src="/images/devmais.png"
-            width={175}
-            height={90}
-            alt="Dev++ logo"
-          />
-        </LinkHeader>
-      </Link>
-      <Navigation />  
-    </HeaderContainer>
-  )
+	return (
+		<HeaderContainer>
+			<Link href="/" passHref>
+				<LinkHeader>
+					<Image
+						src="/images/logo-devmais-removebg-preview.png"
+						width={185}
+						height={55}
+						alt="logo dev++"
+					/>
+				</LinkHeader>
+			</Link>
+			<Navigation />
+		</HeaderContainer>
+	);
 }
