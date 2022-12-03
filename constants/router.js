4b8bf88ffbfbf9489ff3e81e50dc7242cedcbe75
Paulo Sergio <pauloserg861@gmail.com>
2022-12-03 13:18:58 -0300

    criando constant de rotas

---
+++ constants/router.js
@@ -0,0 +1,22 @@
+export const Routers = [
+	{
+		idRouter: 0,
+		path: '/',
+		name: 'Home',
+	},
+	{
+		idRouter: 1,
+		path: '/#about',
+		name: 'Sobre mim',
+	},
+	{
+		idRouter: 2,
+		path: '/#skills',
+		name: 'Minhas Skills',
+	},
+	{
+		idRouter: 3,
+		path: '/#portfolio',
+		name: 'Portfolio',
+	},
+]
