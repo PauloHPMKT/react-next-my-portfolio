b19462bb6c5a225d4d28340abb9e2049e64c617e
Paulo Sergio <pauloserg861@gmail.com>
2022-12-03 13:49:30 -0300

    implementando novas variaveis de estilos

--- src/components/Navigation/Navigation.styles.js
+++ src/components/Navigation/Navigation.styles.js
@@ -12,22 +12,22 @@ export const MenuBar = styled.ul`
 
 export const LinkMenu = styled.a`
 	padding: 15px 20px;
-	font-size: ${(props) => props.theme.fonts.secondary_size};
-	font-weight: ${(props) => props.theme.fonts.fontWeight.light};
-	font-family: ${(props) => props.theme.fonts.textFamily};
+	font-size: ${props => props.theme.fonts.secondary_size};
+	font-weight: ${props => props.theme.fonts.fontWeight.light};
+	font-family: ${props => props.theme.fonts.textFamily};
 	cursor: pointer;
 	transition: 0.4s ease;
 
 	&:hover {
-		color: ${(props) => props.theme.colors.secondary_color};
-		background-color: ${(props) => props.theme.colors.primary1};
-		border-radius: 20px;
+		color: ${props => props.theme.colors.secondary_color};
+		background-color: ${props => props.theme.colors.primary1};
+		border-radius: ${props => props.theme.fonts.secondary_size};
 	}
 
-	@media ${(props) => props.theme.breakpoints.lg} {
+	@media ${props => props.theme.breakpoints.lg} {
 		font-size: 17px;
 	}
-	@media ${(props) => props.theme.breakpoints.md} {
+	@media ${props => props.theme.breakpoints.md} {
 		display: none;
 	}
 `;
@@ -38,12 +38,12 @@ export const MenuMobile = styled.div`
 	right: 20px;
 	z-index: 2000;
 	padding: 10px 15px;
-	background-color: ${(props) => props.theme.colors.primary1};
+	background-color: ${props => props.theme.colors.primary1};
 	border-radius: 10px;
 	font-size: 40px;
-	display: ${(props) => (props.active ? "none" : "flex")};
+	display: ${props => props.active ? "none" : "flex"};
 
-	@media ${(props) => props.theme.breakpoints.md} {
+	@media ${props => props.theme.breakpoints.md} {
 		display: flex;
 	}
 `;
