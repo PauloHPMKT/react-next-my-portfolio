f1c235c46366872a48563d78007a7609de34bfd2
Paulo Sergio <pauloserg861@gmail.com>
2022-12-03 14:27:02 -0300

    ajustando estilizacao do componente

--- src/components/SocialIcons/SocialIcons.styles.js
+++ src/components/SocialIcons/SocialIcons.styles.js
@@ -1,36 +1,37 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-export const MediaIcons = styled.ul `
-    position: absolute; 
-    right: 130px;
-    top: 35%; 
+export const MediaIcons = styled.ul`
+	position: absolute;
+	right: 130px;
+	top: 50%;
+	transform: translateY(-50%);
 
-    @media ${(props => props.theme.breakpoints.xxl)} {
-        top: 250px;
-        right: 70px; 
-    }
+	@media ${(props) => props.theme.breakpoints.xxl} {
+		top: 250px;
+		right: 70px;
+	}
 
-    @media ${(props => props.theme.breakpoints.md)} {
-        right: 20px;
-    }
-    @media ${(props => props.theme.breakpoints.sm)} {
-        right: 0;
-    }
-`
+	@media ${(props) => props.theme.breakpoints.md} {
+		right: 20px;
+	}
+	@media ${(props) => props.theme.breakpoints.sm} {
+		right: 0;
+	}
+`;
 
-export const ListIcons = styled.li `
-    padding: 5px;
-`
+export const ListIcons = styled.li`
+	padding: 5px;
+`;
 
-export const LinkIcon = styled.a `
-    font-size: 26px;
-    transition: .6s ease;
-    padding: 12px;
-    border: 3px solid transparent;
-    display: flex;
+export const LinkIcon = styled.a`
+	font-size: 40px;
+	transition: 0.6s ease-in-out;
+	padding: 12px;
+	border: 3px solid transparent;
+	display: flex;
 
-    &:hover {
-        border: 3px solid #b10101;
-        border-radius: 100%;
-    }
-`
+	&:hover {
+		border: 3px solid ${props => props.theme.colors.primary1};
+		border-radius: 100%;
+	}
+`;
