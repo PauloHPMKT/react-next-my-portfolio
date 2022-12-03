8432ec945343009f3d8252dc84b9bce2e05863eb
Paulo Sergio <pauloserg861@gmail.com>
2022-12-03 16:08:22 -0300

    resstruturando secao de sobre mim

--- src/components/About/About.styles.js
+++ src/components/About/About.styles.js
@@ -1,16 +1,15 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-export const ImageAbout = styled.img `
-    width: 400px;
-    margin: 10px 0;
-    border-radius: 40px;
-    transition: .4s ease;
+export const ImageAbout = styled.img`
+	width: 70%;
+	border-radius: 40px;
+	transition: 0.4s ease;
 
-    &:hover {
-        box-shadow: 0 0 15px;
-    } 
+	&:hover {
+		box-shadow: 0 0 15px;
+	}
 
-    @media ${(props => props.theme.breakpoints.md)} {
-        width: 300px;
-    }
-`
+	@media ${(props) => props.theme.breakpoints.md} {
+		width: 300px;
+	}
+`;
--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,40 +1,40 @@
 import Button from "../Button"
 
-import { 
-  Section, 
-  ContentSection, 
-  SectionDivider, 
-  SectionSubTitle, 
-  ImageContainer, 
-  SectionInto, 
-  SectionText 
+import {
+  Section,
+  ContentSection,
+  SectionDivider,
+  SectionSubTitle,
+  ImageContainer,
+  SectionInto,
+  SectionText
 } from "../../styles/globalComponentsStyles"
 import { ImageAbout } from "./About.styles"
 
 export default function About() {
   return(
-    <Section id="about"> 
+    <Section id="about">
       <SectionDivider />
-      <SectionSubTitle size>
+      <SectionSubTitle onSize>
         Um pouco sobre mim
       </SectionSubTitle>
       <ContentSection row>
         <SectionInto center>
-          <SectionText size>
+          <SectionText onSize>
             Comecei no design em meados de 2007, então entrei no mundo freelancer prestando meus serviços de criação e edição gráfica. Após alguns anos decidi profissionalizar meus serviços.
             <br/><br/>
             Adquirindo novas skills mergulhei no mundo do desenvolvimento web e venho adquirindo habilidades incríveis às quais compartilho com vocês, com o objetivo ajudar aqueles que sonham em andar pela trilha da programação! Fiquem a vontade e vamos incrementar conhecimento juntos!! Dev++.
           </SectionText>
-          <Button onClick={() => {       
-            window.location = `/portfolio`  
+          <Button onClick={() => {
+            window.location = `/portfolio`
           }}>
             Meus projetos
           </Button>
         </SectionInto>
-        <ImageContainer center>
-          <ImageAbout src="/images/about.jpeg" alt="image Paulo" />
-        </ImageContainer>
+				<ImageContainer center startPosition>
+					<ImageAbout src="/images/about.jpeg" alt="image Paulo" />
+				</ImageContainer>
       </ContentSection>
     </Section>
   )
-} 
+}
