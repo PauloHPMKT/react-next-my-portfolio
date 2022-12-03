ac97fda03aa8e58103a17470ec271775f954db6b
Paulo Sergio <pauloserg861@gmail.com>
2022-12-03 13:45:00 -0300

    configurando tipografia da navbar

--- src/components/Navigation/Navigation.styles.js
+++ src/components/Navigation/Navigation.styles.js
@@ -1,49 +1,49 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-export const Navigate = styled.nav `
-    display: flex;
-`
+export const Navigate = styled.nav`
+	display: flex;
+`;
 
-export const MenuBar = styled.ul `
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
+export const MenuBar = styled.ul`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
 
-export const LinkMenu = styled.a `
-    padding: 15px 20px;
-    font-size: ${props => props.theme.fonts.secondary_size};
-    font-weight: 600;
-    cursor: pointer;
-    transition: .4s ease;
+export const LinkMenu = styled.a`
+	padding: 15px 20px;
+	font-size: ${(props) => props.theme.fonts.secondary_size};
+	font-weight: ${(props) => props.theme.fonts.fontWeight.light};
+	font-family: ${(props) => props.theme.fonts.textFamily};
+	cursor: pointer;
+	transition: 0.4s ease;
 
-    &:hover {
-        color: ${props => props.theme.colors.secondary_color};
-        background-color: ${props => props.theme.colors.primary1};
-        border-radius: 20px;
-    }
+	&:hover {
+		color: ${(props) => props.theme.colors.secondary_color};
+		background-color: ${(props) => props.theme.colors.primary1};
+		border-radius: 20px;
+	}
 
-    @media ${(props => props.theme.breakpoints.lg)} {
-        font-size: 17px;
-    }
-    @media ${(props => props.theme.breakpoints.md)} {
-        display: none;
-    }
-`
+	@media ${(props) => props.theme.breakpoints.lg} {
+		font-size: 17px;
+	}
+	@media ${(props) => props.theme.breakpoints.md} {
+		display: none;
+	}
+`;
 
-export const MenuMobile = styled.div `
-    position: fixed;
-    top: 20px;
-    right: 20px;
-    z-index: 2000;
-    padding: 10px 15px;
-    background-color: ${props => props.theme.colors.primary1};
-    border-radius: 10px;
-    font-size: 40px;
-    display: ${props => props.active ? 'none' : 'flex'};
+export const MenuMobile = styled.div`
+	position: fixed;
+	top: 20px;
+	right: 20px;
+	z-index: 2000;
+	padding: 10px 15px;
+	background-color: ${(props) => props.theme.colors.primary1};
+	border-radius: 10px;
+	font-size: 40px;
+	display: ${(props) => (props.active ? "none" : "flex")};
 
-    @media ${(props => props.theme.breakpoints.md)} {
-        display: flex;
-    }
-`
-
+	@media ${(props) => props.theme.breakpoints.md} {
+		display: flex;
+	}
+`;
