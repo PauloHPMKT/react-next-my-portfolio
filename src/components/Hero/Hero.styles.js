e1f8ab4157af8ea6df2ad1aaa961ec2c2416c289
Paulo Sergio <pauloserg861@gmail.com>
2022-12-03 13:24:37 -0300

    importando arquivos estaticos

--- src/components/Hero/Hero.styles.js
+++ src/components/Hero/Hero.styles.js
@@ -1,56 +1,56 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-export const ImageHero = styled.img `
-    width: 420px;
-    border-radius: 100%;
-    border: 10px solid #333333;
-    transition: .6s all;
-    animation: floating 2s ease-in-out infinite;
-    /*animation floating img*/
-    @keyframes floating {
-        0% {
-            transform: translatey(0px);
-        }
-        50% {
-            transform: translatey(-8px);
-        }
-        100% {
-            transform: translatey(0px);
-        }
-    }
+export const ImageHero = styled.div`
+	padding: 10px;
+	background-color: #009acc;
+	width: 520px;
+	height: 550px;
+	transition: 0.6s all;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	/*animation floating img*/
+	animation: floating linear infinite 2s;
+	@keyframes floating {
+		0% {
+			transform: translatey(0px);
+		}
+		50% {
+			transform: translatey(-8px);
+		}
+		100% {
+			transform: translatey(0px);
+		}
+	}
 
-    &:hover {
-        box-shadow: 0 0 20px;
-    }
-
-    @media ${(props => props.theme.breakpoints.mxl)} {
-        width: 350px; 
-    }
-    @media ${(props => props.theme.breakpoints.lg)} {
-        width: 300px; 
-    }
-    @media ${(props => props.theme.breakpoints.sm)} {
-        width: 250px; 
-    }
-`
-
+	@media ${(props) => props.theme.breakpoints.mxl} {
+		width: 350px;
+	}
+	@media ${(props) => props.theme.breakpoints.lg} {
+		width: 300px;
+	}
+	@media ${(props) => props.theme.breakpoints.sm} {
+		width: 250px;
+	}
+`;
 
 export const SectionTitle = styled.h1`
-  font-weight: 800;
-  font-size: ${props => props.main ? '75px' : '60px'};
-  width: max-content;
-  max-width: 100%;
-  background: linear-gradient(121.57deg, #FFFFFF 40.77%, #b10101 60.15%);
-  -webkit-background-clip: text;
-  background-clip: text;
-  -webkit-text-fill-color: transparent;  
-  margin-bottom: 20px;
+	font-weight: 800;
+	font-size: ${(props) => (props.main ? "75px" : "60px")};
+	width: max-content;
+	max-width: 100%;
+	background-color: #fff;
+	//background: linear-gradient(121.57deg, #FFFFFF 40.77%, #b10101 60.15%);
+	-webkit-background-clip: text;
+	background-clip: text;
+	-webkit-text-fill-color: transparent;
+	margin-bottom: 20px;
 
-  @media ${(props => props.theme.breakpoints.mxl)} {
-    font-size: ${(props => props.main ? '59px' : '50px')};
-  }
+	@media ${(props) => props.theme.breakpoints.mxl} {
+		font-size: ${(props) => (props.main ? "59px" : "50px")};
+	}
 
-  @media ${(props => props.theme.breakpoints.xs)} {
-    font-size: 45px;
-  }
-`
+	@media ${(props) => props.theme.breakpoints.xs} {
+		font-size: 45px;
+	}
+`;
--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,16 +1,20 @@
+import Image from "next/image"
 import Button from "../Button"
 import SocialIcons from "../SocialIcons"
 
 import {
-  Section, 
-  ContentSection, 
-  SectionText, 
-  SectionInto, 
-  ImageContainer 
+  Section,
+  ContentSection,
+  SectionText,
+  SectionInto,
+  ImageContainer
 } from "../../styles/globalComponentsStyles"
 import { SectionTitle, ImageHero } from "./Hero.styles"
 
+import PersonalImage from '../../../public/images/pic-avatar.png'
+
 export default function Hero() {
+
   return(
     <Section>
       <ContentSection row>
@@ -19,7 +23,9 @@ export default function Hero() {
             Paulo Sérgio
           </SectionTitle>
           <SectionText size>
-            Olá tudo bem? Sou web desenvolvedor especializado em Front-end com uso da tecnologia React JS. Juntos podemos criar soluções inteligentes.
+            Olá tudo bem? Sou desenvolvedor web focado na área de Front-end utilizando tecnologias da stack de JavaScript como React e Vue JS. Juntos podemos criar soluções inteligentes.
+						<br/><br/>
+						Conheça mais sobre meus trabalhos!
           </SectionText>
           <Button onClick={(e) => {
             e.preventDefault()
@@ -29,7 +35,13 @@ export default function Hero() {
           </Button>
         </SectionInto>
         <ImageContainer center>
-          <ImageHero src="/images/avatar-pic.png" alt="avatar Paulo" />
+					<ImageHero>
+						<Image
+							src={PersonalImage}
+							layout="fill"
+							alt="ola"
+						/>
+					</ImageHero>
         </ImageContainer>
       </ContentSection>
       <SocialIcons />
